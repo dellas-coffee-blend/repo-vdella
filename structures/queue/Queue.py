1ce9b10ede2b01ac5254b571aa84f984ff4ed924
Vitor Della Torre dos Santos <vitor.origamer@gmail.com>
2020-08-14 20:24:44 -0300

    Deleção de package structures

--- structures/queue/Queue.py
+++
@@ -1,7 +0,0 @@
-# coding=utf-8
-from structures.templates.DequeueTemplate import Dequeue
-
-
-# Representação da estrutura de dados Fila -> First In, First Out (FIFO)
-class Queue(Dequeue):
-    pass
--- structures/stack/Stack.py
+++
@@ -1,7 +0,0 @@
-# coding=utf-8
-from structures.templates.DequeueTemplate import Dequeue
-
-
-# Estrutura de dados First-in First-Out (FIFO)
-class Stack(Dequeue):
-    pass
