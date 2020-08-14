53fee20cf19a727abb2e14bc4fc6bbf7b3ab91e7
Vitor Della Torre dos Santos <vitor.origamer@gmail.com>
2020-08-14 14:03:50 -0300

    Redução à classe vazia para treino

--- structures/queue/Queue.py
+++ structures/queue/Queue.py
@@ -4,22 +4,4 @@ from structures.templates.DequeueTemplate import Dequeue
 
 # Representação da estrutura de dados Fila -> First In, First Out (FIFO)
 class Queue(Dequeue):
-
-    def forward_positions(self):
-        i = 0
-        forwarding = self.elements()
-        for _ in range(self.size() - 1):
-            self._elements[i] = forwarding[i + 1]
-            i = i + 1
-
-    def enqueue(self, new_node):
-        return self.add_node(new_node)
-
-    def dequeue(self):
-
-        if self.is_empty():
-            raise ValueError("Não se pode tirar elementos de uma fila vazia. ")
-
-        head = self.elements()[0]
-        self.forward_positions()
-        return head, self
+    pass
--- structures/stack/Stack.py
+++ structures/stack/Stack.py
@@ -4,15 +4,4 @@ from structures.templates.DequeueTemplate import Dequeue
 
 # Estrutura de dados First-in First-Out (FIFO)
 class Stack(Dequeue):
-
-    def push(self, new_node):
-        return self.add_node(new_node)
-
-    def pop(self):
-
-        if self.is_empty():
-            raise ValueError("Uma pilha vazia não pode remover elementos. ")
-
-        pop = self._elements[self.last_index()]
-        self.__size = self.__size - 1
-        return pop, self
+    pass
