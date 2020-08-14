c5a13e9c67826f1dff54a6c4853d22cc2d519579
Vitor Della Torre dos Santos <vitor.origamer@gmail.com>
2020-08-14 20:39:39 -0300

    Criados exercícios para números

---
+++ exercises/numbers.py
@@ -0,0 +1,40 @@
+# coding=utf-8
+
+# Exercício 1:
+# Jorge quer escrever um programa que retorna um valor booleano
+# caso um dado número, enviado pelo usuário, seja um número primo
+# ou não
+def is_a_prime_number(number):
+    """
+    TODO
+     Complete a função de modo que se "number" for primo seja
+     retornado True.
+     Caso contrário, retorne False.
+    """
+    return number
+
+# Exercício 2:
+# A famosa Fabiana foi a uma festa com o intuito de ficar com Jorge, da situação problema anterior.
+# Jorge é uma pessoa difícil e pediu que Fabiana resolvesse o seguinte exercício para decidir.
+def fib(number):
+    """
+    TODO
+     Você deve arranjar uma forma de retornar o fibonacci de um valor chamado "number".
+     Há muitas, muitas formas desse exercício ser realizado, sendo a mais otimizada pouco trivial e, portanto,
+     não é o que queremos no momento. QUERO QUE SEJA RESOLVIDO DE FORMA ITERATIVA
+     OU RECURSIVA DO JEITO QUE ACHAR MELHOR.
+    """
+    return number
+
+# Exercício 3:
+# 30 anos depois de Fabiana e Jorge se casarem, seu filho Matheus pede ajuda num problema:
+# ele relembra dos exercícios que seus pais fizeram e gostaria de saber se poderiam ser combinados.
+# Fabiana e Jorge ficam receosos e hesitantes, mas aceitam o desafio pelo bem da educação de seu filho.
+def prime_number_fib(number):
+    """
+    TODO
+     Será dado um número qualquer à função.
+     1. SE ELE FOR PRIMO, calcule o fibonacci dele e retorne o valor calculado.
+     2. SE ELE NÃO FOR PRIMO, retorne -1.
+    """
+    return number
