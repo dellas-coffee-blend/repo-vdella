34522aed214fc3eba859023fbaf993eb747a7f67
Vitor Della Torre dos Santos <vitor.origamer@gmail.com>
2020-08-14 23:16:40 -0300

    Adicionados exercícios sobre listas

---
+++ exercises/arrays.py
@@ -0,0 +1,40 @@
+# coding=utf-8
+
+# Exercício 1:
+# Giovanna adora programar e gosta, particularmente, de listas.
+# Ela tem tentado responder à seguinte questão:
+# O que irá acontecer se ela tentar fazer a seguinte operação?
+# A = [0, 1]
+# B = ["Arthur"]
+# Retorne A + B
+# Dará certo? Dará errado?
+
+
+# Exercício 2:
+# Arthur é empenhado nos estudos da escola. Professora Fabiana ficou impressionada
+# por sua diligência e lhe disse: "Bill Gates na sua idade soube resolver este problema",
+# e lhe disse qual era este. Fabiana colocou uma carga psicológica demasiadamente grande nos ombros
+# de Arthur. Ele sucumbirá à pressão? Ele irá desistir? Num ímpeto, Arthur disse que conseguia. Eis
+# seu desafio.
+def make_matrix(upper_limit, dimension):
+    """
+    TODO
+     Você deve criar uma matriz "dimension" x "dimension", totalmente preenchida por elementos aleatórios,
+     em que o limite superior destes (ou seja, o valor do qual não podem passar) seja a variável "upper_limit".
+     Ao popular a matriz de números, deve SOMAR OS NÚMEROS DE SUA DIAGONAL PRINCIPAL e retornar o resultado da soma.
+    """
+    return upper_limit, dimension
+
+
+# Exercício 3:
+# Arthur ingressou em Harvard, conheceu Bill Gates e herdou todas as ações da Microsoft. No entanto, há algo que
+# por anos lhe incomodou: o exercício da professora Fabiana tinha uma próxima questão, que era
+# CALCULAR O DETERMINANTE DE UMA MATRIZ.
+# Arthur confia em seu potencial e nunca esquecera do enunciado inteiro da questão. Sabia o que tinha que fazer.
+# Colocando seu cargo de CEO em risco, respondeu à lápis à questão.
+def det(matrix):
+    """
+    TODO
+     Dada uma matriz passada como parâmetro, calcule o determinante desta.
+    """
+    return matrix
