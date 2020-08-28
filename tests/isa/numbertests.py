a0e7613b48c9dcb0f255cd3b1214ab350e615fd6
Vitor Della Torre dos Santos <vitor.origamer@gmail.com>
2020-08-28 20:06:23 -0300

    Adição de novos testes para exercícios numéricos

--- tests/isa/numbertests.py
+++ tests/isa/numbertests.py
@@ -18,3 +18,21 @@ class TestNumbers(unittest.TestCase):
         self.assertFalse(prime_number_fib(11), -1)
         self.assertTrue(prime_number_fib(23), 28657)
         self.assertFalse(prime_number_fib(22), 17711)
+
+    def test_area(self):
+        def test_circle_area():
+            self.assertTrue(area(0, 7.8), 191.134497044)
+            self.assertTrue(area(0, 10.97), 378.062087366)
+            self.assertTrue(area(0, 1234.76), 4789773.4999)
+
+        def test_triangle_area():
+            self.assertTrue(area(1, 8.765, 7.999), 35.0556175)
+            self.assertTrue(area(1, 9776.0006, 1122.055), 5484605.17662)
+
+        def test_square_area():
+            self.assertTrue(area(2, 987.7666), 975682.856076)
+            self.assertTrue(area(2, 10294.0333), 105967121.582)
+
+        test_circle_area()
+        test_triangle_area()
+        test_square_area()
